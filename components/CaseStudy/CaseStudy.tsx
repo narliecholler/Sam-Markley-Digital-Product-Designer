60c62e3eb64553f9b24f3ee3a0ec20f1477a03b3
Charlie Noller <charlie.noller@zoopla.co.uk>
2022-12-11 14:16:50 +0000

    no ts on casestudy

--- components/CaseStudy/CaseStudy.tsx
+++ components/CaseStudy/CaseStudy.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+// @ts-nocheck
 import { useLayoutEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ArrowRightIcon } from "public/assets/icons";
