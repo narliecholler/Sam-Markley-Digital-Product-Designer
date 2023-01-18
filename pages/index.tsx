423d87451a03dae4661fbde9aaf4213d425ba302
Charlie Noller <charlie.noller@zoopla.co.uk>
2023-01-18 15:00:31 +0000

    gsap test

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,11 @@ const Home: NextPage = () => {
 
       <Hero />
 
-      <ThreeDee />
+      {/* <ThreeDee />
 
-      <CSSection />
+      <CSSection /> */}
 
-      {/* <WorkProcesses /> */}
+      <WorkProcesses />
     </>
   );
 };
