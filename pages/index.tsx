2b071cead60bea01a2733d11a576f9749977e7ea
Charlie Noller <charlie.noller@zoopla.co.uk>
2023-02-14 15:38:46 +0000

    fix import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import { motion } from "framer-motion";
 import Head from "next/head";
 import Hero from "@/components/Hero";
 import ThreeDee from "@/components/3D/3D";
